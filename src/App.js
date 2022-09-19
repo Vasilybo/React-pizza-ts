2cc2efeef57c7bff55600bf4fc11e2394c8abbca
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-19 14:20:51 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './scss/app.scss'
+import Header from './components/Header'
+import Categories from './components/Categories'
+import SortBy from './components/SortBy'
+import PizzaBlock from "./components/PizzaBlock";
+
+function App() {
+  return (
+      <div className="wrapper">
+          <Header/>
+          <div className="content">
+              <div className="container">
+                  <div className="content__top">
+                      <Categories/>
+                      <SortBy/>
+                  </div>
+                  <h2 className="content__title">Все пиццы</h2>
+                  <div className="content__items">
+                      <PizzaBlock />
+                  </div>
+              </div>
+          </div>
+      </div>
+  );
+}
+
+export default App;
