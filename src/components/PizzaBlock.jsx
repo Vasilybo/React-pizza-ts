a2a175bbe5b994fdd69bf5c6e85ff9f228f3166c
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-19 17:42:24 +0300

    added state for PizzaBlock component

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const PizzaBlock = (props) => {
+const PizzaBlock = ({ title, price }) => {
+    const [pizzaCount, setPizzaCount] = useState(0)
+
+    const addPizzaCount = () => {
+        return setPizzaCount(pizzaCount + 1);
+    }
     return (
         <div className="pizza-block">
             <img
@@ -8,7 +13,7 @@ const PizzaBlock = (props) => {
                 src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
                 alt="Pizza"
             />
-            <h4 className="pizza-block__title">{props.title}</h4>
+            <h4 className="pizza-block__title">{title}</h4>
             <div className="pizza-block__selector">
                 <ul>
                     <li className="active">тонкое</li>
@@ -22,8 +27,10 @@ const PizzaBlock = (props) => {
                 </ul>
             </div>
             <div className="pizza-block__bottom">
-                <div className="pizza-block__price">от {props.price} ₽</div>
-                <div className="button button--outline button--add">
+                <div className="pizza-block__price">от {price} ₽</div>
+                <button
+                    onClick={addPizzaCount}
+                    className="button button--outline button--add">
                     <svg
                         width="12"
                         height="12"
@@ -37,8 +44,8 @@ const PizzaBlock = (props) => {
                         />
                     </svg>
                     <span>Добавить</span>
-                    <i>2</i>
-                </div>
+                    <i>{pizzaCount}</i>
+                </button>
             </div>
         </div>
     );
