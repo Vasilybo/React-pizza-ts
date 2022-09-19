bf1e664d82725a14140a1c8c710305f45b132254
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-19 18:51:36 +0300

    added state for Categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Categories() {
+    const [activeCategory, setActiveCategory] = useState(0)
+
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые',]
+
+    const onClickCategory = (index) => {
+        setActiveCategory(index)
+    }
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {categories.map((cate, index) => (
+                    <li
+                        onClick={() => onClickCategory(index)}
+                        className={activeCategory === index ? 'active' : ''}
+                    >{cate}
+                    </li>
+                ))}
             </ul>
         </div>
     )
