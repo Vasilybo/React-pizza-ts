4eebee97c0e47e4f4b6b04e159c40e2c836c1422
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-22 16:24:38 +0300

    added styled component NotFoundBlock

---
+++ src/components/NotFoundBlock/NotFoundBlock.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NotFoundBlock = () => {
+    return (
+        <div>
+            <h1>
+                <span>
+                    😕
+                </span>
+                Ничего не найдено
+            </h1>
+        </div>
+    );
+};
+
+export default NotFoundBlock;
