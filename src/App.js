6e71058be2a0ccaa652f8f8e53c97e5b6ac19fa7
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-22 13:00:01 +0300

    added fake server for response of useEffect and deleted json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import {useEffect, useState} from "react";
+
 import './scss/app.scss'
 import Header from './components/Header'
 import Categories from './components/Categories'
 import SortBy from './components/SortBy'
 import PizzaBlock from "./components/PizzaBlock";
 
-import pizzas from './assets/pizzas.json'
+
 
 function App() {
+
+    const [pizzas, setPizzas] = useState([])
+
+    useEffect(() => {
+        fetch('https://632c28bf5568d3cad87e6524.mockapi.io/pizzas').then(response => {
+            return response.json()
+        }).then(json => {
+            setPizzas(json)
+        })
+    }, [])
+
   return (
       <div className="wrapper">
           <Header/>
