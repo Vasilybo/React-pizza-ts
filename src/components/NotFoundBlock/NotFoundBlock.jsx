3f7df96c1ab113a02e97d8715d4358f1d6253712
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-22 16:47:08 +0300

    refactor styled component NotFoundBlock

--- src/components/NotFoundBlock/NotFoundBlock.jsx
+++ src/components/NotFoundBlock/NotFoundBlock.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
+import styles from './NotFoundBlock.module.scss'
+
 const NotFoundBlock = () => {
     return (
-        <div>
+        <div className={styles.root}>
             <h1>
                 <span>😕</span>
                 <br/>
                 Ничего не найдено
             </h1>
+            <p className={styles.description}>К сожалению, данная страница отсутствует в приложении</p>
         </div>
     );
 };
