794653fd54b5c416d33b2b43296c6fb09b7f8894
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-22 17:02:59 +0300

    added Link to Home page for icon pizza

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import pizzaLogo from '../assets/img/pizza-logo.svg'
+import {Link} from "react-router-dom";
 
 function Header() {
     return (
         <div className="header">
             <div className="container">
-                <div className="header__logo">
-                    <img width="38" src={pizzaLogo} alt="Pizza logo"/>
-                    <div>
-                        <h1>React Pizza</h1>
-                        <p>самая вкусная пицца во вселенной</p>
+                <Link to={'/'}>
+                    <div className="header__logo">
+                        <img width="38" src={pizzaLogo} alt="Pizza logo"/>
+                        <div>
+                            <h1>React Pizza</h1>
+                            <p>самая вкусная пицца во вселенной</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
                 <div className="header__cart">
                     <a href="/cart.html" className="button button--cart">
                         <span>520 ₽</span>
