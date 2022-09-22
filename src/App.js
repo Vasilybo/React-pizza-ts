6028c1ff0e627c638d8ee6415e62a35f184a6e7a
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-22 16:59:59 +0300

    added Routes for 2 pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import {Routes, Route} from "react-router-dom";
+
 import './scss/app.scss'
 import Header from './components/Header'
+import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
 
 function App() {
@@ -8,7 +11,10 @@ function App() {
             <Header/>
             <div className="content">
                 <div className="container">
-                    <Home/>
+                    <Routes>
+                        <Route path='/' element={<Home/>}/>
+                        <Route path='*' element={<NotFound/>}/>
+                    </Routes>
                 </div>
             </div>
         </div>
