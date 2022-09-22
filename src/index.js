e183352371e0bf33faa2e39a25aabc257dd73d14
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-22 15:53:12 +0300

    added BrowserRouter for App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
