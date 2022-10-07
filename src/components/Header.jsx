3ba6783e919f1a19f626420783aeb96f4837d28f
Vasilybo <swagrec.vasilybo@gmail.com>
2022-10-07 12:49:54 +0300

    added new component Search input

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import pizzaLogo from '../assets/img/pizza-logo.svg'
 import {Link} from "react-router-dom";
+import Search from "./Search";
 
 function Header() {
     return (
@@ -15,6 +16,7 @@ function Header() {
                         </div>
                     </div>
                 </Link>
+                <Search/>
                 <div className="header__cart">
                     <Link to={'/cart'} className="button button--cart">
                         <span>520 ₽</span>
