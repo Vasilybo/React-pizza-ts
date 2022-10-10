2c6029a958ef6993329a6999dceb804c1e4ae893
Vasilybo <swagrec.vasilybo@gmail.com>
2022-10-10 18:05:06 +0300

    added onChangePage for Pagination

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactPaginate from "react-paginate";
 import styles from "./Pagination.module.scss"
 
-const Pagination = () => {
+const Pagination = ({ onChangePage }) => {
     return (
         <ReactPaginate
             className={styles.root}
             breakLabel="..."
             nextLabel=">"
             previousLabel="<"
-            onPageChange={event => console.log(event)}
-            pageRangeDisplayed={8}
+            onPageChange={event => onChangePage(event.selected + 1)}
+            pageRangeDisplayed={4}
             pageCount={3}
             renderOnZeroPageCount={null}
         />
