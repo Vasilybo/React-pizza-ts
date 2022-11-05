7f6cc6f0350995384b81de1569e3274c3c5826c2
Vasilybo <swagrec.vasilybo@gmail.com>
2022-11-05 12:39:17 +0300

    imported const store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 
 import {BrowserRouter} from "react-router-dom";
 
+import { store } from "./redux/store";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
