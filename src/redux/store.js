1a66354c9d468d37891c50f4d8ce3399e6b53f63
Vasilybo <swagrec.vasilybo@gmail.com>
2022-11-06 15:09:24 +0300

    refactor store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import filter from "./slices/filterSlice";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        filter
+    },
 })
