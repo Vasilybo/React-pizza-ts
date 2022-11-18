1039ace13c053337139a3af9549c745f46e68226
Vasilybo <swagrec.vasilybo@gmail.com>
2022-11-18 17:39:04 +0300

    added eventListener and DidMount, UnMount with useEffect hook for sort popup

--- src/components/SortBy.jsx
+++ src/components/SortBy.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { useSelector, useDispatch} from "react-redux";
 import { setSelectedSort } from "../redux/slices/filterSlice";
 
@@ -12,6 +12,7 @@ function SortBy() {
 
     const dispatch = useDispatch()
     const sort = useSelector(state => state.filter.sort)
+    const sortRef = useRef()
     const [isVisible, setIsVisible] = useState(false)
 
     const onClickSortName = (obj) => {
@@ -19,8 +20,21 @@ function SortBy() {
         setIsVisible(false)
     }
 
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if(!event.path.includes(sortRef.current)) {
+                setIsVisible(false)
+            }
+        }
+        document.body.addEventListener('click', handleClickOutside)
+
+        return () => {
+            document.body.removeEventListener('click', handleClickOutside)
+        }
+    }, [])
+
     return (
-        <div className="sort">
+        <div ref={sortRef} className="sort">
             <div className="sort__label">
                 <svg
                     width="10"
