5950aed2406aa5d9331b89d5dde69b95cf2f13e7
Vasilybo <swagrec.vasilybo@gmail.com>
2022-11-21 15:56:41 +0300

    refactor reducers method for minimize array of object in cart

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -10,6 +10,19 @@ export const cartSlice = createSlice({
     initialState,
     reducers: {
         addProduct (state, action) {
+            const findProduct = state.items.find((obj) => obj.id === action.payload.id)
+
+            if (findProduct) {
+                findProduct.count++
+            } else {
+                state.items.push({
+                    ...action.payload,
+                    count: 1,
+                });
+                state.totalPrice = state.items.reduce((sum, obj) => {
+                            return (obj.price * obj.count) + sum
+                        }, 0)
+            }
             state.items.push(action.payload);
             state.totalPrice = state.items.reduce((sum, obj) => {
                 return obj.price + sum
