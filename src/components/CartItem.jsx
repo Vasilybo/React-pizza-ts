b18faa439300035b558ea6a182bb10bfba9cbc28
Vasilybo <swagrec.vasilybo@gmail.com>
2022-11-24 18:04:31 +0300

    added props for CartItem

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function CartItem() {
+function CartItem({ id, name, count, imageUrl}) {
     return (
         <div className="cart__item">
             <div className="cart__item-img">
