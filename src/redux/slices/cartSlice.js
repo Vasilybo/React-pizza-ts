84ebb22b9a8dbac3a82e8cf4f7087ea3053219c9
Vasilybo <swagrec.vasilybo@gmail.com>
2022-11-24 17:51:44 +0300

    refactor cartSlice method addProduct and added 2 new methods for push array to cart

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -5,31 +5,49 @@ const initialState = {
     items: [],
 }
 
-export const cartSlice = createSlice({
+const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        addProduct (state, action) {
-            const findProduct = state.items.find((obj) => obj.id === action.payload.id)
-
-            if (findProduct) {
-                findProduct.count++
-            } else {
-                state.items.push({
-                    ...action.payload,
-                    count: 1,
-                });
-                state.totalPrice = state.items.reduce((sum, obj) => {
-                            return (obj.price * obj.count) + sum
-                        }, 0)
-            }
+        addProduct(state, action) {
             state.items.push(action.payload);
             state.totalPrice = state.items.reduce((sum, obj) => {
                 return obj.price + sum
             }, 0)
         },
+
+        // addProduct (state, action) {
+        //     const findProduct = state.items.find((obj) => obj.id === action.payload.id)
+        //
+        //     if (findProduct) {
+        //         findProduct.count++;
+        //     } else {
+        //         state.items.push({
+        //             ...action.payload,
+        //             count: 1,
+        //         });
+        //     }
+        //         state.totalPrice = state.items.reduce((sum, obj) => {
+        //             return obj.price * obj.count + sum
+        //         }, 0)
+        // },
+
+        // addProduct(state, action) {
+        //     const findProduct = state.items.find(obj => {
+        //         return ((obj.id === action.payload.id) &&
+        //             (obj.size === action.payload.size) &&
+        //             (obj.type === action.payload.type))
+        //     });
+        //     findProduct ? findProduct.count++ : state.items.push({
+        //         ...action.payload,
+        //         count: 1,
+        //     });
+        //     state.totalPrice = state.items.reduce((sum, obj) => {
+        //         return obj.price * obj.count + sum;
+        //     }, 0);
+        // },
         removeProduct (state, action) {
-            state.items = state.items.filter(obj => obj.id !== action.payload);
+            state.items = state.items.filter((obj) => obj.id !== action.payload);
         },
         clearProduct (state) {
             state.items = [];
