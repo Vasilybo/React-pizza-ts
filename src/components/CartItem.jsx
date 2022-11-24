37c92c6edd81760e8b86a46ec5f51249e7c9e2a8
Vasilybo <swagrec.vasilybo@gmail.com>
2022-11-24 18:09:28 +0300

    added imageUrl

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -6,7 +6,7 @@ function CartItem({ id, name, type, price, count, imageUrl}) {
             <div className="cart__item-img">
                 <img
                     className="pizza-block__image"
-                    src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+                    src={imageUrl}
                     alt="Pizza"
                 />
             </div>
