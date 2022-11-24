1ef424d7aa0f8d69804ced79b576279d18d836a6
Vasilybo <swagrec.vasilybo@gmail.com>
2022-11-24 18:06:42 +0300

    added count for b

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -22,7 +22,7 @@ function CartItem({ id, name, type, count, imageUrl}) {
                     </svg>
 
                 </div>
-                <b>2</b>
+                <b>{count}</b>
                 <div className="button button--outline button--circle cart__item-count-plus">
                     <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z" fill="#EB5A1E"/>
