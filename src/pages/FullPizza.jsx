76ebc8ba0baa6006ecd57db17a743a893d39d00a
Vasilybo <swagrec.vasilybo@gmail.com>
2023-02-06 16:14:41 +0300

    added className for div

--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FullPizza = () => {
     return (
-        <div>
+        <div className="container">
             <img src=""/>
             <h2>2222</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet corporis culpa dolores fugit quidem ut. Aliquid dolor est ex illo mollitia nesciunt quasi quos tenetur voluptatum. Nihil, unde, ut.</p>
