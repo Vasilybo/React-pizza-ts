bf371408f55383bf8419bad8cc6454331d6144d8
Vasilybo <swagrec.vasilybo@gmail.com>
2023-02-06 16:11:54 +0300

    added new component FullPizza

---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FullPizza = () => {
+    return (
+        <div>
+            <img src=""/>
+            <h2>2222</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet corporis culpa dolores fugit quidem ut. Aliquid dolor est ex illo mollitia nesciunt quasi quos tenetur voluptatum. Nihil, unde, ut.</p>
+            <h4>250</h4>
+        </div>
+    );
+};
+
+export default FullPizza;
