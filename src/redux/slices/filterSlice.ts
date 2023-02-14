2ffb9f9225be850d46d9a14348b2d638a07251d3
Vasilybo <swagrec.vasilybo@gmail.com>
2023-02-14 15:32:10 +0300

    refactor sortProperty

--- src/redux/slices/filterSlice.ts
+++ src/redux/slices/filterSlice.ts
@@ -3,7 +3,7 @@ import {RootState} from "../store";
 
 type Sort = {
     name: string;
-    sortProperty: 'rating';
+    sortProperty: 'rating' | 'title' | 'price';
 }
 
 interface FilterSliceState {
