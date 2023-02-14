537804d81853115af6c458e8f23a22dd06505a8a
Vasilybo <swagrec.vasilybo@gmail.com>
2023-02-14 13:52:38 +0300

    typed event onChange

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -25,7 +25,7 @@ const  Search = () => {
         [],
     )
 
-    const onChangeInput = (event: any) => {
+    const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
         setValue(event.target.value)
         updateSearchValue(event.target.value)
     }
