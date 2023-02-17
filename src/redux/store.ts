6826f958893c369c5291a8311954ea8fa589ca83
Vasilybo <swagrec.vasilybo@gmail.com>
2023-02-17 12:44:29 +0300

    added typed dispatch

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import filter from "./slices/filterSlice";
 import cart from "./slices/cartSlice";
 import pizzas from "./slices/pizzasSlice";
+import {useDispatch} from "react-redux";
 
 export const store = configureStore({
     reducer: {
@@ -12,3 +13,6 @@ export const store = configureStore({
 })
 
 export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch: () => AppDispatch = useDispatch
